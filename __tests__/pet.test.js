811b944a440ac408b0f0b86e0629d807ce5fbaac
Josh Bamford <joshbamford09@gmail.com>
2022-10-21 21:38:28 +0100

    completed setting the name of the pet and being able to retrieve it

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,8 +1,19 @@
 const Pet = require ('../src/pet');
 
-
 describe('constructor', () => {
     it('returns an object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
+    });
+});
+describe('constructor', () => {
+    it('Sets the name property', () => {
+        const pet = new Pet('Fido');
+        expect(pet.name).toEqual('Fido')
+    });
+});
+describe('constructor', () => {
+    it('has an initial age of 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.age).toEqual(0)
     });
 });
